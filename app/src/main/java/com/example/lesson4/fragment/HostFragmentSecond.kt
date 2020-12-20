3e81a52ed34709c36d93f1dc1efb32ed833c9541
PavelShramko <pavel.shramko.97@mail.ru>
2020-12-20 21:00:39 +0300

    v2

--- app/src/main/java/com/example/lesson4/fragment/HostFragmentSecond.kt
+++ app/src/main/java/com/example/lesson4/fragment/HostFragmentSecond.kt
@@ -7,14 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.lesson4.R
 
-class HostFragmentSecond : Fragment(R.layout.fragment_second) {
+class HostFragmentSecond : Fragment(R.layout.fragment_cont_second) {
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_second, container, false)
+        val view = inflater.inflate(R.layout.fragment_cont_second, container, false)
         return view
     }
 
